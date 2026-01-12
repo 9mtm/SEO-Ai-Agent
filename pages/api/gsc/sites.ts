40513155cc840c725bcf08b9fc18503d1f106378
Mohamed Alarade <mohameed.tofeek88@gmail.com>
2026-01-13 00:02:19 +0100

    oner

--- pages/api/gsc/sites.ts
+++ pages/api/gsc/sites.ts
@@ -34,11 +34,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         console.log('[GSC Sites] Total sites from Google:', sites.length);
         console.log('[GSC Sites] All sites with permissions:', JSON.stringify(sites, null, 2));
 
-        // Filter to only sites where user is Owner
-        // Only owners have full permissions to access Search Console data
+        // Filter to only sites where user has full permissions
+        // Include Owner, Full User, and Unverified User
         const ownerSites = sites.filter((site: any) =>
             site.permissionLevel === 'siteOwner' ||
-            site.permissionLevel === 'siteUnverifiedUser' // Sometimes this also works
+            site.permissionLevel === 'siteFullUser' ||
+            site.permissionLevel === 'siteUnverifiedUser'
         );
 
         console.log('[GSC Sites] Owner sites after filter:', ownerSites.length);
